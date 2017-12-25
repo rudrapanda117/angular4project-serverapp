f88fd1a4a18bd7ad9d8298ec337a73f04b2e9941
Rudrasish <rudrapanda117@gmail.com>
2017-12-25 11:36:21 +0530

    Input decorator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,17 @@ import { FormsModule } from '@angular/forms';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { AppComponent } from './app.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
