f7d4dbc670a46e85641159e6055969c7412c85f5
Rudrasish <rudrapanda117@gmail.com>
2017-12-25 18:34:09 +0530

    HostListner

--- src/app/render-highlighter/render-highlighter.directive.ts
+++ src/app/render-highlighter/render-highlighter.directive.ts
@@ -1,17 +1,26 @@
 
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+
 
 
 @Directive({
   selector: '[appRenderHighlighter]'
 })
-export class RenderHighlighterDirective implements OnInit{
+export class RenderHighlighterDirective implements OnInit {
 
   constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
 
 
 
     public ngOnInit(): void {
-       this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+       // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    }
+
+    @HostListener('mouseenter') mouseenter( eventData: Event) {
+      this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    }
+
+    @HostListener('mouseleave') mouseout( eventData: Event) {
+      this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'white');
     }
 }
