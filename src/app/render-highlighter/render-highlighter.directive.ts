ee79ebbdd262b3a730a35c355bd1dde0857a7730
Rudrasish <rudrapanda117@gmail.com>
2017-12-25 18:46:02 +0530

    HostBinding

--- src/app/render-highlighter/render-highlighter.directive.ts
+++ src/app/render-highlighter/render-highlighter.directive.ts
@@ -1,5 +1,11 @@
-
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  OnInit,
+  Renderer2
+} from '@angular/core';
 
 
 
@@ -8,19 +14,22 @@ import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular
 })
 export class RenderHighlighterDirective implements OnInit {
 
-  constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
 
+  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
 
+  public ngOnInit(): void {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+  }
 
-    public ngOnInit(): void {
-       // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-    }
+  @HostListener('mouseenter') mouseenter(eventData: Event) {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = 'blue';
+  }
 
-    @HostListener('mouseenter') mouseenter( eventData: Event) {
-      this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-    }
+  @HostListener('mouseleave') mouseout(eventData: Event) {
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'white');
+    this.backgroundColor = 'transparent';
 
-    @HostListener('mouseleave') mouseout( eventData: Event) {
-      this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'white');
-    }
+  }
 }
