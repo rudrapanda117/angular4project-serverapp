e19db7293df2487e16d3f614076484bad6ca1815
Rudrasish <rudrapanda117@gmail.com>
2017-12-26 20:04:23 +0530

    Fetching Route parameter reactively

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,4 @@
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -12,6 +12,13 @@ export class UserComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe( (response: Params) => {
+      this.user = {
+        id: response.get('id') ,
+        name: response.get('name')
+      };
+    });
+
     this.user = {
       id: this.route.snapshot.params['id'] ,
       name: this.route.snapshot.params['name']
