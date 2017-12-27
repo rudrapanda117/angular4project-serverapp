6421258ce028a9673679a07eb316cd00c8e5ae24
Rudrasish <rudrapanda117@gmail.com>
2017-12-27 17:37:20 +0530

    Observable start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,82 +1,22 @@
-import { ServerResolver } from './servers/server/server-resolver.service';
-import { AuthService } from './auth.service';
-import {
-  AppRoutingModule
-} from './app-routing.module';
-import {
-  BrowserModule
-} from '@angular/platform-browser';
-import {
-  NgModule
-} from '@angular/core';
-import {
-  FormsModule
-} from '@angular/forms';
-import {
-  HttpModule
-} from '@angular/http';
-import {
-  Route,
-  RouterModule,
-  Routes
-} from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {
-  AppComponent
-} from './app.component';
-import {
-  HomeComponent
-} from './home/home.component';
-import {
-  UsersComponent
-} from './users/users.component';
-import {
-  ServersComponent
-} from './servers/servers.component';
-import {
-  UserComponent
-} from './users/user/user.component';
-import {
-  EditServerComponent
-} from './servers/edit-server/edit-server.component';
-import {
-  ServerComponent
-} from './servers/server/server.component';
-import {
-  ServersService
-} from './servers/servers.service';
-import {
-  PageNotFoundComponent
-} from './page-not-found/page-not-found.component';
-import { AuthGaurd } from './auth-gaurd.service';
-import { CanDeactivateGaurd } from './servers/edit-server/can-deactivate-gaurd.service';
-import { ErrorPageComponent } from './error-page/error-page.component';
-
-
-
-
-
-
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    UsersComponent,
-    ServersComponent,
-    UserComponent,
-    EditServerComponent,
-    ServerComponent,
-    PageNotFoundComponent,
-    ErrorPageComponent
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
     AppRoutingModule
   ],
-  providers: [ServersService, AuthService, AuthGaurd, CanDeactivateGaurd, ServerResolver],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,4 @@
-import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth.service';
-
 
 @Component({
   selector: 'app-home',
@@ -10,21 +7,9 @@ import { AuthService } from '../auth.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  onLoadServer(id: number) {
-    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
-  }
-
-  onLogIn() {
-    this.authService.logIn();
-  }
-
-  onLogOut() {
-    this.authService.logOut();
   }
 
 }
